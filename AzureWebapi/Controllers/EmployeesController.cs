0993679ec8d48ce4b9f6c65e133bddea0976eeb9
vijaya72569 <vijayaias2017@gmail.com>
2025-11-12 15:49:11 +0530

    Add webapiazure

---
+++ AzureWebapi/Controllers/EmployeesController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureWebapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly Models.EmpDbContext _context;
+        public EmployeesController(Models.EmpDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var employees = _context.Employees.ToList();
+            return Ok(employees);
+        }
+        [HttpPost]
+        public IActionResult Post(Models.Employee employee)
+        {
+            _context.Employees.Add(employee);
+            _context.SaveChanges();
+            return Ok(employee);
+        }
+        [HttpPut]
+        public IActionResult Put(Models.Employee employee)
+        {
+            var existingEmployee = _context.Employees.Find(employee.Id);
+            if (existingEmployee == null)
+            {
+                return NotFound();
+            }
+            existingEmployee.Name = employee.Name;
+            existingEmployee.Salary = employee.Salary;
+            _context.SaveChanges();
+            return Ok(existingEmployee);
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var employee = _context.Employees.Find(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            _context.Employees.Remove(employee);
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ AzureWebapi/Models/EmpDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AzureWebapi.Models
+{
+    public class EmpDbContext: DbContext
+    {
+        public EmpDbContext(DbContextOptions<EmpDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
